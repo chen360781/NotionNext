996c692e588b3f0514ffdb30cf24bef29daf805b
chen360781 <120346974+chen360781@users.noreply.github.com>
2024-07-15 00:07:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,6 +28,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://hao.cwh360781.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '备多分, 博客', // 网站关键词 英文逗号隔开
+  SINCE: process.env.NEXT_SINCE || 2024, // 建站年份
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '133717056@qq.com', // 邮箱地址 例如mail@tangly1024.com
